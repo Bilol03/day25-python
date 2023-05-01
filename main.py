7c596f6d9c66c5f09be867ba6ac17fc20c5fe1d5
Bilol Madiyarov <bilol@Bilols-MacBook-Air.local>
2023-05-02 03:52:37 +0900

    Coverted temperatures that are taken from csv file from celsius to fahrenheit

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import pandas as pd
-
+#
 df = pd.read_csv('./weather_data.csv')
+#
+# print(df['temp'])
+#
+# data_dict = df.to_dict()
+# print(data_dict)
+#
+# data_list = df['temp'].to_list()
+#
+# print(data_list)
+#
+# avr = df['temp'].mean()
+#
+# print(avr)
+#
+# max_value = df['temp'].max()
+#
+# print(max_value)
+#
 
-print(df['temp'])
 
-data_dict = df.to_dict()
-print(data_dict)
+# Celsius to Fahrenheit
 
-data_list = df['temp'].to_list()
+celsius = df['temp']
 
-print(data_list)
+for i in celsius:
+    fahrenhit = (i * 9/5) + 32
 
-avr = df['temp'].mean()
-
-print(avr)
-
-max_value = df['temp'].max()
-
-print(max_value)
-
+    print(fahrenhit)
 
